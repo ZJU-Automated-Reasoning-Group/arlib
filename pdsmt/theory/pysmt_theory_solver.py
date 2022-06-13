38304683208da2deafb03d985377d8c09b0a71bc
rainoftime <rainoftime@gmail.com>
2022-06-13 20:04:06 +0800

    init pysmt_th

--- pdsmt/theory/pysmt_theory_solver.py
+++ pdsmt/theory/pysmt_theory_solver.py
@@ -1,8 +1,32 @@
 # coding: utf-8
 import logging
+from pysmt.logics import AUTO
+# from pysmt.oracles import get_logic
+# from pysmt.shortcuts import EqualsOrIff
+from pysmt.shortcuts import Solver, Portfolio, get_model
+from pysmt.shortcuts import Symbol, Bool, And, Not
+from pysmt.typing import INT, REAL
 
 """
 Wrappers for PySMT
 """
 
 logger = logging.getLogger(__name__)
+
+
+class PySMTTheorySolver(object):
+
+    def __init__(self):
+        self.solver = Solver()
+
+    def add(self, smt2string):
+        raise NotImplementedError
+
+    def check_sat(self):
+        raise NotImplementedError
+
+    def check_sat_assuming(self, assumptions):
+        raise NotImplementedError
+
+    def get_unsat_core(self):
+        raise NotImplementedError
