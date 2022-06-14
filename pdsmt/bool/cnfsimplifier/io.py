9795311b044da23a3d75e91691cdef2802a2ec54
rainoftime <rainoftime@gmail.com>
2022-06-14 15:58:19 +0800

    rm unused features

---
+++ pdsmt/bool/cnfsimplifier/io.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+from os import path
+import os
+from .variable import Variable
+from .cnf import Cnf
+from .clause import Clause
+
+
+class NumericClausesReader:
+    def __init__(self):
+        pass
+
+    def read(self, clauses):
+        clause_list = list()
+        for cls in clauses:
+            var_list = list()
+            for var in cls:
+                if var != 0:
+                    var_list.append(Variable(var))
+            clause_list.append(Clause(var_list))
+        cnf = Cnf(clause_list)
+        return cnf
