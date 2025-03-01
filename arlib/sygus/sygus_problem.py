a8d6fcdba1953d292dc0b992b2e18b8989679385
rainoftime <rainoftime@gmail.com>
2025-03-02 00:26:59 +0800

    remove unused

--- arlib/sygus/sygus_problem.py
+++ arlib/sygus/sygus_problem.py
@@ -1,12 +1,22 @@
 
-from enum import Enum, auto
 
-class SyGuSProblemType:
-    # CLIA, SyGuS-PBE, InvSyn..? (following the categories
-    # of SyGuS-COMP
-    CLIA = auto()
-    PBE = auto()
-    INVSYN = auto()
+class SygusParser:
+    def __init__(self, problem_str):
+        self.problem_str = problem_str
+        self.problem_str = self.problem_str.replace("\n", "")
+        self.problem_str = self.problem_str.replace(" ", "")
+        self.problem_str = self.problem_str.replace("(", "")
+        self.problem_str = self.problem_str.replace(")", "")
 
-    
+    def parse(self):
+        """
+        :return:
+        """
+        tokens = self.problem_str.split(";")
+        assert len(tokens) == 2
+        assert tokens[0] == "check-synth"
+        assert tokens[1].startswith("(= (")
+        tokens = tokens[1].split(")")
+        assert len(tokens) == 2
+
     
