4d86e0b1151c249234b40d723ce5e480edbe919b
rainoftime <rainoftime@gmail.com>
2025-03-23 20:28:02 +0800

    Enhance testing framework with new test cases for value conversion and regex matching. Update pytest configuration to include slow test marker. Refactor setup dependencies by commenting out unused packages. Improve regex for SMT-LIB value extraction to support additional types.

--- arlib/utils/__init__.py
+++ arlib/utils/__init__.py
@@ -3,5 +3,3 @@ from .sexpr import parse_sexpr_string
 from .types import SolverResult
 from .values import RE_GET_EXPR_VALUE_ALL
 
-SolverResult = SolverResult
-RE_GET_EXPR_VALUE_ALL = RE_GET_EXPR_VALUE_ALL
